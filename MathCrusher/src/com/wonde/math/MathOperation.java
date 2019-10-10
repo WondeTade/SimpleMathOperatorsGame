8a52497c298438065ff4f2540c882fd4a2b06925
Bubu <Bubu@BuLiMa>
2019-10-10 16:05:42 +0200

    New Math operator class is added

---
+++ MathCrusher/src/com/wonde/math/MathOperation.java
@@ -0,0 +1,17 @@
+package com.wonde.math;
+
+public class MathOperation {
+	
+	int add;
+	int minus;
+	double div;
+	int mult;
+	
+	public MathOperation (int add, int minus, double div, int mult) {
+		this.add = add;
+		this.minus = minus;
+		this.div = div;
+		this.mult = mult;
+	}
+
+}
